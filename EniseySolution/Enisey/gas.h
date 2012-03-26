ac7da7f2c27c71614c9c6937929b2d2fecd2b969
Alexey Vasilyev <bestspajic@gmail.com>
2012-03-26 23:26:15 +0400

    Take newest version of gas.h from London - default cinstructors

--- EniseySolution/Enisey/gas.h
+++ EniseySolution/Enisey/gas.h
@@ -2,6 +2,7 @@
 
 struct GasWorkParameters
 {
+  GasWorkParameters() : p(0), t(0), q(0) { };
   float p;	// Давление, [МПа]
   float t;	// Тепереатура, [К]
   float q;	// Количество газа, [млн м3/сут]
@@ -9,6 +10,7 @@ struct GasWorkParameters
 
 struct GasCompositionReduced
 {
+  GasCompositionReduced() : density_std_cond(0), n2(0), co2(0) { };
   float density_std_cond;		// Плотность при стд. усл-ях [кг/м3]
   float n2;					// Доля азота [б.р.]
   float co2;					// Доля углекислого газа [б.р.]
