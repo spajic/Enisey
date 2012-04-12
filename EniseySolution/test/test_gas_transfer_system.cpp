bc4f1c13cf6e60c81604208028922e7562ac4934
Alexey Vasilyev <bestspajic@gmail.com>
2012-04-12 19:23:39 +0400

    Test FindsBalanceForSaratovGorkiy.

--- EniseySolution/test/test_gas_transfer_system.cpp
+++ EniseySolution/test/test_gas_transfer_system.cpp
@@ -8,6 +8,8 @@
 #include <string>
 #include <iostream>
 #include <list>
+#include <fstream>
+#include <iomanip>
 
 const std::string path_to_vesta_files = "C:\\Enisey\\data\\saratov_gorkiy\\";
 
@@ -40,36 +42,79 @@ TEST_F(GasTransferSystemFromVestaTest, MakesInitialApprox) {
   /// \todo Добавить автоматические проверки на корректность.
   gts.WriteToGraphviz(graphviz_filename);
 }
-TEST_F(GasTransferSystemFromVestaTest, CountsAllEdges) {
+TEST_F(GasTransferSystemFromVestaTest, FindsBalanceForSaratovGorkiy) {
   gts.MakeInitialApprox();
   gts.CountAllEdges();
   gts.MixVertices();
   const std::string graphviz_filename = 
     "C:\\Enisey\\out\\GTSCountsAllEdges.dot";
   /// \todo Добавить автоматические проверки на корректность.
+
+  std::list<double> int_disbs;
+  std::list<double> abs_disbs;
+
   gts.WriteToGraphviz(graphviz_filename);
   double d = gts.CountDisbalance();
-  std::cout << "Disb1 = " << d << std::endl;
-  std::list<double> disbs;
+  abs_disbs.push_back(d);
+  int_disbs.push_back( gts.GetIntDisbalance() );
   double g = 1.0 / 2.0;
   double d_prev = 1000000;
   gts.SetSlaeRowNumsForVertices();
   for(int n = 0; n < 35; ++n) {
     gts.CountNewIteration(g);
-    double d = gts.CountDisbalance();
+    d = gts.CountDisbalance();
+    abs_disbs.push_back(d);
     if(d_prev < d) {
       g *= 0.9;
     }
     d_prev = d;
     int d_int = gts.GetIntDisbalance();
-    std::cout << d << " " << d_int << std::endl;
+    int_disbs.push_back(d_int);
     if(d_int == 0) {
       break;
     }
-    disbs.push_back(d);
   }
-  /*for(auto d = disbs.begin(); d != disbs.end(); ++d) {
-    std::cout << *d << std::endl;
-  }*/
   gts.WriteToGraphviz("C:\\Enisey\\out\\Result.dot");
+
+  // Файл, в который запишем эталонную последовательность дисбалансов для
+  // дальнейшего сравнения. Эта функциональность включается только тогда,
+  // когда нужно сформировать новый эталон.
+  // Файл имеет формат
+  // на строчке: номер итерации, дисбаланс, число узлов c дисбалансом.
+  const std::string etalon_saratov_gorkiy_balance = 
+    "C:\\Enisey\\out\\SaratovGorkiy\\etalon_balance_find.txt";
+//#define NEW_ETALON
+#ifdef NEW_ETALON
+  std::ofstream etalon_f(etalon_saratov_gorkiy_balance); 
+  etalon_f << std::setprecision(18) << std::fixed;
+  auto i_d = int_disbs.begin();
+  auto a_d = abs_disbs.begin();
+  for(unsigned int iter_num = 0; iter_num < abs_disbs.size(); ++iter_num) {
+    etalon_f << iter_num << " " << *a_d << " " << *i_d << std::endl;
+    ++a_d;
+    ++i_d;
+  }
+#endif  
+  // Загружаем информацию об эталоне и сравниваем с фактом.
+#ifndef NEW_ETALON
+  std::ifstream etalon_f(etalon_saratov_gorkiy_balance);
+  std::list<double> etalon_abs_disbs;
+  std::list<double> etalon_int_disbs;
+  int et_iter(0);
+  double et_abs_d(0.0);
+  double et_int_d(0.0);
+  while(etalon_f >> et_iter >> et_abs_d >> et_int_d) {
+    etalon_abs_disbs.push_back(et_abs_d);
+    etalon_int_disbs.push_back(et_int_d);
+  }
+  ASSERT_EQ( abs_disbs.size(), etalon_abs_disbs.size() );
+  bool abs_disbs_equal = std::equal(
+      etalon_abs_disbs.begin(), etalon_abs_disbs.end(),
+      abs_disbs.begin() );
+  bool int_disbs_equal = std::equal(
+      etalon_int_disbs.begin(), etalon_int_disbs.end(),
+      int_disbs.begin() );
+  EXPECT_TRUE(abs_disbs_equal);
+  EXPECT_TRUE(int_disbs_equal);
+#endif
 }
