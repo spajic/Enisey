4559ab30a0697d9e24102879bf4f15d924e48b60
Alexey Vasilyev <bestspajic@gmail.com>
2012-11-15 00:19:44 +0400

    ParallelManagerPipeCUDA - except calculating q.
    Make all work, except actual q calculating in kernel.
    Need to sync q calculating in CPU and CUDA.

--- EniseySolution/Enisey/parallel_manager_pipe_cuda.cu
+++ EniseySolution/Enisey/parallel_manager_pipe_cuda.cu
@@ -2,10 +2,18 @@
 Реализация менеджера параллельного моделирования труб, 
 использующего CUDA.*/
 #include "parallel_manager_pipe_cuda.cuh"
-#include "model_pipe_sequential.h"
+
 #include <vector>
+#include "calculated_params.h"
+
 // Для векторных типов CUDA double2, double4 и т.д.
 #include <vector_types.h>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include "model_pipe_sequential_functions_cuda.cuh"
+#include "gas_count_functions_cuda.cuh"
 
 ParallelManagerPipeCUDA::ParallelManagerPipeCUDA() {
   number_of_pipes = 0;
@@ -13,21 +21,158 @@ ParallelManagerPipeCUDA::ParallelManagerPipeCUDA() {
 void ParallelManagerPipeCUDA::
   TakeUnderControl(std::vector<PassportPipe> const &passports) { 
     number_of_pipes = passports.size();
+    AllocateMemoryOnHost();
+    SavePassportsAsStructureOfArrays(passports);    
     AllocateMemoryOnDevice();
+    SendPassportsToDevice();
 }
 void ParallelManagerPipeCUDA::
-  SetWorkParams(std::vector<WorkParams> const &work_params) {    
-
+    SetWorkParams(std::vector<WorkParams> const &work_params) {    
+  SaveWorkParamsToStructureOfArrays(work_params);
+  SendWorkParamsToDevice();
 }
 void ParallelManagerPipeCUDA::CalculateAll() {
-
+  // Вызов соответствующих CUDA-kernel.
+  SendCalculatedParamsToHost();
 }
 void ParallelManagerPipeCUDA::
-  GetCalculatedParams(std::vector<CalculatedParams> *calculated_params){
-
+    GetCalculatedParams(std::vector<CalculatedParams> *calculated_params){
+  calculated_params->erase(
+      calculated_params->begin(), calculated_params->end() );
+  calculated_params->reserve(number_of_pipes);
+  for(auto q = q_result_host.begin(); q != q_result_host.end(); ++q) {
+    CalculatedParams cp;
+    cp.set_q(*q);
+    calculated_params->push_back(cp);
+  }
 }
 
+void ParallelManagerPipeCUDA::AllocateMemoryOnHost() {
+  // Пасспортные параметры.
+  length_host               .reserve(number_of_pipes);
+  d_in_out_host             .reserve(number_of_pipes);
+  hydr_rough_env_exch_host  .reserve(number_of_pipes);
+  // Рабочие параметры.
+  den_sc_host               .reserve(number_of_pipes);
+  co2_host                  .reserve(number_of_pipes);
+  n2_host                   .reserve(number_of_pipes);
+  p_in_and_t_in_host        .reserve(number_of_pipes);
+  p_target_host             .reserve(number_of_pipes);
+  // Рассчитанные параметры.
+  q_result_host             .reserve(number_of_pipes);
+}
+void ParallelManagerPipeCUDA::AllocateMemoryOnDevice() {
+  // Пасспортные параметры. 
+  length_dev_vec              .reserve(number_of_pipes);
+  d_in_out_dev_vec            .reserve(number_of_pipes);
+  hydr_rough_env_exch_dev_vec .reserve(number_of_pipes);
+  // Рабочие параметры.
+  p_in_and_t_in_dev_vec       .reserve(number_of_pipes);
+  p_target_dev_vec            .reserve(number_of_pipes);  
+  den_sc_dev_vec              .reserve(number_of_pipes);
+  co2_dev_vec                 .reserve(number_of_pipes);
+  n2_dev_vec                  .reserve(number_of_pipes);
+  // Рассчитанные параметры.
+  q_result_dev_vec            .reserve(number_of_pipes);
+}
 void ParallelManagerPipeCUDA::
-  AllocateMemoryOnDevice() {
-  //cudaMalloc( (void**)&(length_dev_), number_of_pipes * sizeof(double) );
+    SavePassportsAsStructureOfArrays(
+        std::vector<PassportPipe> const &passports) {
+  for(auto p = passports.begin(); p != passports.end(); ++p) {
+    double length = p->length_;
+    double2 d_in_out = make_double2(p->d_inner_, p->d_outer_);
+    double4 hydr_rough_env_exch = 
+        make_double4(
+            p->hydraulic_efficiency_coeff_,
+            p->roughness_coeff_,
+            p->t_env_,
+            p->heat_exchange_coeff_ );
+    length_host.push_back(length);
+    d_in_out_host.push_back(d_in_out);
+    hydr_rough_env_exch_host.push_back(hydr_rough_env_exch);
+  }
+}
+void ParallelManagerPipeCUDA::
+    SaveWorkParamsToStructureOfArrays(
+        std::vector<WorkParams> const &work_params){
+  int index_in_vector = 0;
+  for(auto wp = work_params.begin(); wp != work_params.end(); ++wp) {
+    den_sc_host       [index_in_vector] = wp->den_sc_in();
+    co2_host          [index_in_vector] = wp->co2_in();
+    n2_host           [index_in_vector] = wp->n2_in();
+    p_in_and_t_in_host[index_in_vector] = 
+                            make_double2( wp->p_in(), 
+                                          wp->t_in() );
+    p_target_host     [index_in_vector] = wp->p_out();
+    ++index_in_vector;
+  }
+}
+
+void ParallelManagerPipeCUDA::SendPassportsToDevice() {
+  // Магия thrust. Присваивание dev_vec = host_vec отправляет вектор на Device.
+  length_dev_vec              = length_host;
+  d_in_out_dev_vec            = d_in_out_host;
+  hydr_rough_env_exch_dev_vec = hydr_rough_env_exch_host;
+}
+void ParallelManagerPipeCUDA::SendWorkParamsToDevice() {
+  den_sc_dev_vec        = den_sc_host;
+  co2_dev_vec           = co2_host;
+  n2_dev_vec            = n2_host;
+  p_in_and_t_in_dev_vec = p_in_and_t_in_host;
+  p_target_dev_vec      = p_target_host;
+}
+void ParallelManagerPipeCUDA::SendCalculatedParamsToHost() {
+  q_result_host = q_result_dev_vec;
+}
+
+__global__ 
+void FindQResultCudaKernel(
+	int size,
+	double* den_sc, double* co2, double* n2, 
+	double2* p_and_t, double* p_target,
+	double* length,
+	double2* d_in_out,
+	double4* hydr_rough_env_exch,
+	double* q_result
+)
+{
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+  while(index < size)
+	{
+		// Загружаем данные
+		// Состав газа
+		double den_sc_ = den_sc[index];
+		double co2_ = co2[index];
+		double n2_ = n2[index];
+		// Давление и температура на входе
+		double2 p_and_t_ = p_and_t[index];
+		// Пасспотные параметры трубы
+		double length_ = length[index];
+		double2 d_in_out_ = d_in_out[index];
+		double4 hydr_rough_env_exch_ = hydr_rough_env_exch[index];
+		double p_target_ = p_target[index];
+		
+		// Вычисляем базовые свойства газового потока
+		double r_sc_ = FindRStandartConditionsCuda(den_sc_); // Совпадает c CPU.
+		double t_pc_ = FindTPseudoCriticalCuda(den_sc_, co2_, n2_); // Совпадает.
+		double p_pc_ = FindPPseudoCriticalCuda(den_sc_, co2_, n2_); // Совпадает.
+
+		double q_out = 0;
+		double p_out = 0;
+		double t_out = 0;
+		
+		FindSequentialQCudaRefactored(
+			 p_target_,
+			 p_and_t_.x,  p_and_t_.y,  // рабочие параметры газового потока на входе
+			 p_pc_,  t_pc_,  r_sc_,  den_sc_,
+			 d_in_out_.x,  d_in_out_.y,  hydr_rough_env_exch_.y,  hydr_rough_env_exch_.x, // св-ва трубы
+			 hydr_rough_env_exch_.z,  hydr_rough_env_exch_.w, // св-ва внешней среды (тоже входят в пасспорт трубы)
+			 length_/10, 10, // длина сегмента и кол-во сегментов
+			 &p_out, &t_out,
+			 &q_out); // out - параметры, значения на выходе 
+
+		q_result[index] = q_out;
+		
+		index += gridDim.x * blockDim.x;
+	} // end while (index < size)
 }
--- EniseySolution/Enisey/parallel_manager_pipe_cuda.cuh
+++ EniseySolution/Enisey/parallel_manager_pipe_cuda.cuh
@@ -5,10 +5,11 @@
 
 #include <vector>
 
-//Forward-declarations:
-class ModelPipeSequential;
-class double2;
-class double4;
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+using thrust::host_vector;
+using thrust::device_vector;
 
 class ParallelManagerPipeCUDA : public ParallelManagerPipeI {
 public:
@@ -18,30 +19,44 @@ public:
   virtual void CalculateAll();
   virtual void GetCalculatedParams(std::vector<CalculatedParams> *calculated_params);
 private:  
+  void AllocateMemoryOnHost();
   void AllocateMemoryOnDevice();
+  void SavePassportsAsStructureOfArrays(
+      std::vector<PassportPipe> const &passports);
+  void SaveWorkParamsToStructureOfArrays(
+      std::vector<WorkParams> const &work_params);
+  void SendPassportsToDevice();
+  void SendWorkParamsToDevice();
+  void SendCalculatedParamsToHost();
+  
 
   int number_of_pipes;
 
-  double* length_dev_;
-  double2* d_in_out_dev_;
-  double4* hydr_rough_env_exch_dev_;
-  double2* p_in_and_t_in_dev_;
-  double* p_target_dev_;
-  double* q_result_dev_;
-  double* den_sc_dev_;
-  double* co2_dev_;
-  double* n2_dev_;
+  // На Device:
+  // Пасспортные параметры.  
+  device_vector<double>   length_dev_vec;
+  device_vector<double2>  d_in_out_dev_vec;
+  device_vector<double4>  hydr_rough_env_exch_dev_vec;
+  // Рабочие параметры.
+  device_vector<double2>  p_in_and_t_in_dev_vec;
+  device_vector<double>   p_target_dev_vec;  
+  device_vector<double>   den_sc_dev_vec;
+  device_vector<double>   co2_dev_vec;
+  device_vector<double>   n2_dev_vec;
+  // Рассчитанные параметры.
+  device_vector<double>   q_result_dev_vec;
 
+  // На Host:
   // Пасспортные параметры трубы
-  double* length_;
-  double2* d_in_out_;
-  double4* hydr_rough_env_exch_;
-
+  host_vector<double>   length_host;
+  host_vector<double2>  d_in_out_host;
+  host_vector<double4>  hydr_rough_env_exch_host;
   // Рабочие параметры
-  double* den_sc_;
-  double* co2_; 
-  double* n2_;
-  //double2* p_in_and_t_in_;
-  double* p_target_;
-  double* q_result_;
+  host_vector<double>   den_sc_host;
+  host_vector<double>   co2_host;
+  host_vector<double>   n2_host;
+  host_vector<double2>  p_in_and_t_in_host;
+  host_vector<double>   p_target_host;
+  // Рассчитанные параметры
+  host_vector<double>   q_result_host;
 };
